c6eb49f69c69805df76940e46727898e01ead919
Alise Otília Ramírez <aliseotiliaramirez@gmail.com>
2019-10-22 17:33:05 -0700

    Add files via upload

--- data-analysis.py
+++ data-analysis.py
@@ -20,8 +20,6 @@ def initial_analysis(df):
     print('Report of Initial Data Analysis:\n')
     print(f'Shape of dataframe: {df.shape}')
     print(f'Features and Data Types: \n {df.dtypes}')
-initial_analysis(df)
-
 
 def percent_missing(df):
     """
@@ -38,11 +36,15 @@ def percent_missing(df):
     perc=[round(df[c].isna().mean()*100,2) for c in col]
     miss_dict=dict(zip(col,perc))
     return miss_dict
-percent_missing(df)
 
+num_feat=[]
+def numerical_features(df):
+    for c in list(df.columns):
+        if (df[c].dtypes) == 'int' or (df[c].dtypes) == 'float':
+            num_feat.append(c)
+    return num_feat
 
-non_gauss=[]
-def sample_normality(df,list_var):
+def sample_normality(df,col_list):
     """
     Given a dataframe determines whether each numerical column is Gaussian 
 
@@ -56,19 +58,18 @@ def sample_normality(df,list_var):
         - W Statistic
         - p-value
         - Acceptance or Rejection of null hypothesis (Ho)
-        - List of columns that are not Gaussian 
+        - List of columns that do not have gaussian distribution
 
     """
     alpha = 0.05
-    # Subset features of dataset that are numerical 
-    for c in list(df.columns):
-        if (df[c].dtypes) == 'int' or (df[c].dtypes) == 'float':
-            stat,p=shapiro(df)
-            if p > alpha:
-                print('Sample distribution is Gaussian (Fail to reject Ho)')
-            else:
-                print('Sample distribution is not Gaussian (Reject Ho)')
-                non_gauss.append(df[c])
+    non_gauss=[]
+    for f in num_feat:
+        stat,p=shapiro(df[f])
+
+        if p > alpha:
+            print('Sample distribution is Gaussian (Fail to reject Ho)')
+        else:
+            print('Sample distribution is not Gaussian (Reject Ho)')
+            non_gauss.append(df[f])
         return stat,p
     return non_gauss
-sample_normality(df,non_gauss)
